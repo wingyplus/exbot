4b695edfb1b86a9ccfb78fe78a478c47d3249ebc
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-03-17 22:47:11 +0700

    Create new tsv file from filename

--- exbot.py
+++ exbot.py
@@ -2,11 +2,13 @@
 
 import sys
 import csv
+from os import path
 from openpyxl import load_workbook
 
-ws = load_workbook(filename=sys.argv[1]).active
+filename = sys.argv[1]
+ws = load_workbook(filename=filename).active
 
-with open("_google-test.tsv", "w") as tsvfile:
+with open("_{name}.tsv".format(name=path.splitext(path.basename(filename))[0]), "w") as tsvfile:
     robotfile = csv.writer(tsvfile, delimiter="\t")
     for row in ws.rows:
         robotfile.writerow([cell.value if cell.value is not None else "" for cell in row])
