8ff6b4eb387c55c1ab51acc1355b2176053abffb
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-03-17 22:59:28 +0700

    Refactoring
    I move write tsv file block to function and check it should be
    called as a command

--- exbot.py
+++ exbot.py
@@ -6,17 +6,24 @@ import csv
 from os import path
 from openpyxl import load_workbook
 
+def create_robot_file(filename):
+    """
+    Generate tsv file and return robotfilename
+    """
+    robotfilename = "_{name}.tsv".format(name=path.splitext(path.basename(filename))[0])
+    ws = load_workbook(filename=filename).active
+
+    with open(robotfilename, "w") as tsvfile:
+        robotfile = csv.writer(tsvfile, delimiter="\t")
+        for row in ws.rows:
+            robotfile.writerow([cell.value if cell.value is not None else "" for cell in row])
+
+    return robotfilename
+
+
 def robot_run(filename):
     subprocess.call("robot {filename}".format(filename=filename), shell=True)
 
-filename = sys.argv[1]
-robotfilename = "_{name}.tsv".format(name=path.splitext(path.basename(filename))[0])
 
-ws = load_workbook(filename=filename).active
-
-with open(robotfilename, "w") as tsvfile:
-    robotfile = csv.writer(tsvfile, delimiter="\t")
-    for row in ws.rows:
-        robotfile.writerow([cell.value if cell.value is not None else "" for cell in row])
-
-robot_run(robotfilename)
+if __name__ == "__main__":
+    robot_run(create_robot_file(sys.argv[1]))
