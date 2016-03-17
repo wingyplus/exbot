7e896ce2ad78107a13f56f390e3bf3bedbfe3c3a
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-03-17 23:06:48 +0700

    Remove robot tsv file after run robot command

--- exbot.py
+++ exbot.py
@@ -3,14 +3,14 @@
 import sys
 import subprocess
 import csv
-from os import path
+import os
 from openpyxl import load_workbook
 
 def create_robot_file(filename):
     """
     Generate tsv file and return robotfilename
     """
-    robotfilename = "_{name}.tsv".format(name=path.splitext(path.basename(filename))[0])
+    robotfilename = "_{name}.tsv".format(name=os.path.splitext(os.path.basename(filename))[0])
     ws = load_workbook(filename=filename).active
 
     with open(robotfilename, "w") as tsvfile:
@@ -26,4 +26,6 @@ def robot_run(filename):
 
 
 if __name__ == "__main__":
-    robot_run(create_robot_file(sys.argv[1]))
+    robotfilename = create_robot_file(sys.argv[1])
+    robot_run(robotfilename)
+    os.remove(robotfilename)
